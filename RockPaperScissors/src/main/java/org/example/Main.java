e081034cefa61db04b2bd9f0c2d708907d10ec1b
Jamming17 <jackchiplin@outlook.com>
2023-11-30 12:34:09 +0000

    Added user choice functionality

--- RockPaperScissors/src/main/java/org/example/Main.java
+++ RockPaperScissors/src/main/java/org/example/Main.java
@@ -5,10 +5,26 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
         System.out.println("Welcome to Rock, Paper, Scissors!");
-        System.out.println("Select an option: ");
-        String userChoice = input.nextLine();
+
+        boolean valid = false;
+        while (valid == false) {
+            System.out.println("Select an option: ");
+            String userChoice = input.nextLine();
+            userChoice = userChoice.toLowerCase();
+            if (userChoice == "s" || userChoice == "scissors") {
+                userChoice = "scissors";
+                valid = true;
+            } else if (userChoice == "p" || userChoice == "paper") {
+                userChoice = "paper";
+                valid = true;
+            } else if (userChoice == "r" || userChoice == "rock") {
+                userChoice = "rock";
+                valid = true;
+            } else {
+                System.out.println("Invalid choice; please enter rock, paper or scissors");
+            }
+        }
 
         //Opponent randomisation
 
