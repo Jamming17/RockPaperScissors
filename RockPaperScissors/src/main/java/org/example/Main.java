bdbc00266e7f3ea4c4de47d2f95685009856e53b
Jamming17 <jackchiplin@outlook.com>
2023-11-30 00:04:27 +0000

    RPS Skeleton (first commit)

---
+++ RockPaperScissors/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Rock, Paper, Scissors!");
+        System.out.println("Select an option: ");
+        // Player options
+
+        //Opponent randomisation
+
+        System.out.println("You selected __");
+        System.out.println("Your opponent selected __");
+        System.out.println("__ wins!");
+        //Maybe implement replayability?
+    }
+}
